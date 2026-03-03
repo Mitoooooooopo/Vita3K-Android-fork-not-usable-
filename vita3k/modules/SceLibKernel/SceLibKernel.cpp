11ccd2191110b745a3967718778e3ab6e4af414e
CrmC <79812109+ikhoeyZX@users.noreply.github.com>
2026-03-04 07:04:35 +0800

    Update SceLibKernel.cpp

--- vita3k/modules/SceLibKernel/SceLibKernel.cpp
+++ vita3k/modules/SceLibKernel/SceLibKernel.cpp
@@ -1110,10 +1110,6 @@ EXPORT(int, sceKernelBacktraceSelf) {
 
 EXPORT(int, sceKernelCallModuleExit) {
     TRACY_FUNC(sceKernelCallModuleExit);
-    LOG_ERROR("App call exit!");
-    SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, "Crashed", "Application crashed!", emuenv.window.get());
-    emuenv.kernel.exit_delete_all_threads();
-    emuenv.load_exec = true;
     
     return 0;
     //return UNIMPLEMENTED();
