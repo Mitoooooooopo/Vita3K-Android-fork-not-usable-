5d461938c57a1e3b33456b1eaacaaa306c8e269d
noobdog667 <106137571+Mitoooooooopo@users.noreply.github.com>
2026-03-11 11:29:19 +0530

    Update texture.cpp

--- vita3k/renderer/src/gl/texture.cpp
+++ vita3k/renderer/src/gl/texture.cpp
@@ -84,7 +84,7 @@ static GLenum bcn_to_rgba8(const SceGxmTextureBaseFormat format) {
     case SCE_GXM_TEXTURE_BASE_FORMAT_SBC4:
         return GL_R8_SNORM;
     case SCE_GXM_TEXTURE_BASE_FORMAT_UBC5:
-        return autoG8;
+        return return GL_RG8;
     case SCE_GXM_TEXTURE_BASE_FORMAT_SBC5:
         return GL_RG8_SNORM;
     default:
@@ -190,8 +190,7 @@ void GLTextureCache::upload_texture_impl(SceGxmTextureBaseFormat base_format, ui
         size_t compressed_size = renderer::texture::get_compressed_size(base_format, width, height);
         glCompressedTexSubImage2D(upload_type, mip_index, 0, 0, width, height,
                                   fmt, static_cast<GLsizei>(compressed_size), pixels);
-    } else if (gxm::is_bcn_format(base_format) || renderer::texture::is_astc_format(base_format)) { {
-        glPixelStorei(GL_UNPACK_ROW_LENGTH, static_cast<GLint>(pixels_per_stride));
+    } else if (gxm::is_bcn_format(base_format) || renderer::texture::is_astc_format(base_format)autoG8       glPixelStorei(GL_UNPACK_ROW_LENGTH, static_cast<GLint>(pixels_per_stride));
 
         if (gxm::is_bcn_format(base_format)) {
             const GLint block_size = (base_format == SCE_GXM_TEXTURE_BASE_FORMAT_UBC1 || base_format == SCE_GXM_TEXTURE_BASE_FORMAT_UBC4 || base_format == SCE_GXM_TEXTURE_BASE_FORMAT_SBC4)
